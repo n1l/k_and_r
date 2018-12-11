70f990d819670d2e2795dccfc52b3b18b93b55b1
n1l <akashtorm@gmail.com>
2018-12-12 00:04:27 +0300

    Change 1.5

--- 1_5.c
+++ 1_5.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
 
+#define UPPER 300
+#define LOWER 0
+#define STEP 20
+
+
 int main()
 {
-
-  // float fahr, celcius;
-  // int lower, upper, step;
-
-  // lower = 0;
-  // upper = 300;
-  // step= 20;
-
-  // fahr = lower;
-
   printf("------------------------\n");
   printf("fahr\t|\tcelcius\n");
   printf("------------------------\n");
 
   int fahr;
-  for(fahr = 300; fahr >= 0; fahr-=20)
-  {
+  for (fahr = UPPER; fahr >= LOWER; fahr-=STEP) {
     printf("%3d\t|\t%6.1f\n", fahr, (5.0/9.0) * (fahr-32.0));
   }
 
