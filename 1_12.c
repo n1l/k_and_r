1255e2701642c45d42c45991bf4b7c64e15e3b24
n1l <akashtorm@gmail.com>
2018-12-12 01:43:37 +0300

    Add 1.12 exercise

---
+++ 1_12.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+  int c;
+  while ((c = getchar()) != EOF) {
+    if (c != ' ') {
+      putchar(c);
+      while ((c = getchar()) != ' ') {
+        if (c == EOF) {
+          break;
+        }
+
+        putchar(c);
+      }
+    }
+    if (c == ' ') {
+      putchar('\n');
+    }
+  }
+}
